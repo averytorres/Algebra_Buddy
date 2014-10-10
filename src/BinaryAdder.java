e9b685c8b747d019d4666c525f54411266e2cfc6
Avery Torres <atorres39@gatech.edu>
2014-10-09 20:40:01 -0500

    refined Binary Adder

--- src/BinaryAdder.java
+++ src/BinaryAdder.java
@@ -15,63 +15,22 @@ public class BinaryAdder {
 		String shorterBinaryNum = String.valueOf(Integer.min(num1, num2));
 		StringBuilder result = new StringBuilder();
 		boolean carry = false;
-		int diff = longerBinaryNum.length() - shorterBinaryNum
-				.length();
-		
-		for (int i = 0; i < diff; i++) {
-			shorterBinaryNum = "0" + shorterBinaryNum;
-		}
+		final int DIFF = longerBinaryNum.length() - shorterBinaryNum.length();
+		final int NUMBER_LENGTH = String.valueOf(longerBinaryNum).length();
+		boolean NumberIsNotZero = Integer.valueOf(longerBinaryNum) > 0;
 
-		if (Integer.valueOf(longerBinaryNum) > 0) {
-			for (int i = 0; i < String.valueOf(longerBinaryNum).length(); i++) {
-				if (longerBinaryNum.charAt(longerBinaryNum.length() - (i + 1)) == '0') {
-					if (shorterBinaryNum.charAt(shorterBinaryNum.length()
-							- (i + 1)) == '0') {
-						// bother are 0
-						if (!carry)
-							result.insert(0, "0");
-						else {
-							result.insert(0, "1");
-							carry = false;
-						}
-					} else {
-						// longer is 0 shorter is 1
-						if (!carry)
-							result.insert(0, "1");
-						else {
-							result.insert(0, "0");
-							carry = true;
-							if (i == longerBinaryNum.length() - 1) {
-								result.insert(0, "1");
-							}
-						}
+		shorterBinaryNum = lengthen(shorterBinaryNum, DIFF);
 
-					}
+		if (NumberIsNotZero) {
+			for (int i = 0; i < NUMBER_LENGTH; i++) {
+				boolean currentLongerPositionValueIsOne = longerBinaryNum
+						.charAt(longerBinaryNum.length() - (i + 1)) == '0';
+				if (currentLongerPositionValueIsOne) {
+					carry = compareToZero(longerBinaryNum, shorterBinaryNum,
+							result, carry, i);
 				} else {
-					if (shorterBinaryNum.charAt(shorterBinaryNum.length()
-							- (i + 1)) == '0') {
-						// longer is 1 shorter is 0
-						if (!carry)
-							result.insert(0, "1");
-						else {
-							result.insert(0, "0");
-							carry = true;
-							if (i == longerBinaryNum.length() - 1) {
-								result.insert(0, "1");
-							}
-						}
-					} else {
-						// both are 1
-						if(carry)
-							result.insert(0, "1");
-						else
-							result.insert(0, "0");
-						
-						carry = true;
-						if (i == longerBinaryNum.length() - 1) {
-							result.insert(0, "1");
-						}
-					}
+					carry = compareToOne(longerBinaryNum, shorterBinaryNum,
+							result, carry, i);
 				}
 			}
 		} else {
@@ -81,10 +40,86 @@ public class BinaryAdder {
 		return Integer.valueOf(result.toString());
 	}
 
-	public static void main(String[] args) {
-		// BinaryAdder b = new BinaryAdder(11100, 1010);
-		BinaryAdder b = new BinaryAdder(11111111, 000);
-		System.out.println(b.add());
+	private boolean compareToOne(String longerBinaryNum,
+			String shorterBinaryNum, StringBuilder result, boolean carry, int i) {
+		boolean currentShorterPositionValueIsZero = shorterBinaryNum
+				.charAt(shorterBinaryNum.length() - (i + 1)) == '0';
+
+		if (currentShorterPositionValueIsZero) {
+			carry = longerZeroShorterOne(longerBinaryNum, result, carry, i);
+		} else {
+			// both are 1
+			carry = bothPositionsAreOne(longerBinaryNum, result, carry, i);
+		}
+		return carry;
 	}
 
-}//0111111110
+	private boolean compareToZero(String longerBinaryNum,
+			String shorterBinaryNum, StringBuilder result, boolean carry, int i) {
+		boolean currentShorterPositionValueIsZero = shorterBinaryNum.charAt(shorterBinaryNum.length() - (i + 1)) == '0';
+		if (currentShorterPositionValueIsZero) {
+			carry = bothPositionsAreZero(result, carry);
+		} else {
+			carry = longerZeroShorterOne(longerBinaryNum, result, carry, i);
+
+		}
+		return carry;
+	}
+
+	private boolean bothPositionsAreOne(String longerBinaryNum,
+			StringBuilder result, boolean carry, int i) {
+		if (carry)
+			insertOneIntoResult(result);
+		else
+			insertZeroIntoResult(result);
+
+		carry = true;
+		if (i == longerBinaryNum.length() - 1) {
+			insertOneIntoResult(result);
+		}
+		return carry;
+	}
+
+	private boolean longerZeroShorterOne(String longerBinaryNum,
+			StringBuilder result, boolean carry, int i) {
+		// longer is 0 shorter is 1
+		if (!carry)
+			insertOneIntoResult(result);
+		else {
+			insertZeroIntoResult(result);
+			carry = true;
+			if (i == longerBinaryNum.length() - 1) {
+				insertOneIntoResult(result);
+			}
+		}
+		return carry;
+	}
+
+	private boolean bothPositionsAreZero(StringBuilder result, boolean carry) {
+		// bother are 0
+		if (!carry)
+			insertZeroIntoResult(result);
+		else {
+			insertOneIntoResult(result);
+			carry = false;
+		}
+		return carry;
+	}
+
+	private String lengthen(String shorterBinaryNum, int diff) {
+		for (int i = 0; i < diff; i++) {
+			shorterBinaryNum = "0" + shorterBinaryNum;
+		}
+		return shorterBinaryNum;
+	}
+
+	private void insertOneIntoResult(StringBuilder result) {
+		result.insert(0, "1");
+	}
+
+	private void insertZeroIntoResult(StringBuilder result) {
+		result.insert(0, "0");
+	}
+
+
+}
